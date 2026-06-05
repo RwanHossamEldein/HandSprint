61341bf1d097254fe5e4a1c246843866b25fa3d1
Rwan Hossam <rwanhossam0@gmail.com>
2026-06-05 19:19:22 +0300

    Add game assets and update asset references in pubspec.yaml

---
+++ lib/const/game_images/game_images.dart
@@ -0,0 +1,9 @@
+class GameImages {
+  static const String background = 'assets/images/background_handsprint.png';
+  static const String coinsAndObstacles = 'assets/images/coins_obstacles.png';
+  static const String player1 = 'assets/images/player1.png';
+  static const String player2 = 'assets/images/player2.png';
+   static const String player3 = 'assets/images/player3.png';
+
+  
+}
